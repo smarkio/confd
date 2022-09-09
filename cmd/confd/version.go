9f0da13f6413d17db263eb9089c9bc82751e8f76
Diogo Teixeira <diogo.teixeira@smark.io>
2022-09-09 10:59:33 +0100

    Update version file.

--- cmd/confd/version.go
+++ cmd/confd/version.go
@@ -1,6 +1,6 @@
 package main
 
-const Version = "0.19.0"
+const Version = "0.20.4"
 
 // We want to replace this variable at build time with "-ldflags -X main.GitSHA=xxx", where const is not supported.
 var GitSHA = ""
